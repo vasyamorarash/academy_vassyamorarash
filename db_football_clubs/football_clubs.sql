6eede5d56f0d1eebdf923b6e5508fdc5238d942f
Vassya Morarash <vassya_07@live.com>
2014-10-26 14:21:59 +0200

    Revert "Update"
    This reverts commit 0440186799f596fb9a469e78a881ee76b9e84707.

--- db_football_clubs/football_clubs.sql
+++ db_football_clubs/football_clubs.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Окт 26 2014 г., 13:27
+-- Время создания: Окт 25 2014 г., 15:55
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -30,19 +30,21 @@ CREATE TABLE IF NOT EXISTS `clubs` (
   `id` int(4) unsigned NOT NULL AUTO_INCREMENT,
   `name` char(50) NOT NULL,
   `town_id` int(4) unsigned NOT NULL,
-  `year` year(4) NOT NULL,
+  `country_id` int(4) unsigned NOT NULL,
+  `year` int(11) NOT NULL,
   `annual_budget` bigint(20) NOT NULL,
   `president_id` int(4) unsigned NOT NULL,
-  `stadium_id` int(4) unsigned NOT NULL,
+  `home_stadium_id` int(4) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `id_2` (`id`),
   UNIQUE KEY `id_3` (`id`),
   UNIQUE KEY `id_4` (`id`),
-  KEY `home_stadion_id` (`stadium_id`),
+  KEY `home_stadion_id` (`home_stadium_id`),
   KEY `town_id` (`town_id`),
+  KEY `country_id` (`country_id`),
   KEY `president_id` (`president_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -58,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `clubs_leagues` (
   UNIQUE KEY `id` (`id`),
   KEY `club_id` (`club_id`),
   KEY `league_id` (`league_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -76,15 +78,15 @@ CREATE TABLE IF NOT EXISTS `clubs_trophys` (
   UNIQUE KEY `id_2` (`id`),
   KEY `club_id` (`club_id`,`trophy_id`),
   KEY `trophy_id` (`trophy_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `countries`
+-- Структура таблицы `countrys`
 --
 
-CREATE TABLE IF NOT EXISTS `countries` (
+CREATE TABLE IF NOT EXISTS `countrys` (
   `id` int(4) unsigned NOT NULL AUTO_INCREMENT,
   `name` char(30) NOT NULL,
   `population` int(11) NOT NULL,
@@ -101,15 +103,13 @@ CREATE TABLE IF NOT EXISTS `countries` (
 CREATE TABLE IF NOT EXISTS `leagues` (
   `id` int(4) unsigned NOT NULL AUTO_INCREMENT,
   `name` char(40) NOT NULL,
-  `country_id` int(4) unsigned NOT NULL,
-  `uefa_rating` int(11) NOT NULL,
-  `president_id` int(4) unsigned NOT NULL,
+  `country` char(30) NOT NULL,
+  `rating_uefa` int(11) NOT NULL,
+  `president_name` char(40) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `id_2` (`id`),
-  KEY `country_id` (`country_id`),
-  KEY `president_id` (`president_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  UNIQUE KEY `id_2` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
@@ -134,12 +134,12 @@ CREATE TABLE IF NOT EXISTS `presidents` (
 CREATE TABLE IF NOT EXISTS `stadiums` (
   `id` int(4) unsigned NOT NULL AUTO_INCREMENT,
   `name` char(40) NOT NULL,
-  `year` year(4) NOT NULL,
+  `year` int(11) NOT NULL,
   `capacity` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `id_2` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -167,11 +167,11 @@ CREATE TABLE IF NOT EXISTS `towns` (
 CREATE TABLE IF NOT EXISTS `trophys` (
   `id` int(4) unsigned NOT NULL AUTO_INCREMENT,
   `name` char(40) NOT NULL,
-  `year` year(4) NOT NULL,
+  `year` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `id_2` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -181,36 +181,30 @@ CREATE TABLE IF NOT EXISTS `trophys` (
 -- Ограничения внешнего ключа таблицы `clubs`
 --
 ALTER TABLE `clubs`
-  ADD CONSTRAINT `clubs_ibfk_4` FOREIGN KEY (`stadium_id`) REFERENCES `stadiums` (`id`),
+  ADD CONSTRAINT `clubs_ibfk_4` FOREIGN KEY (`home_stadium_id`) REFERENCES `stadiums` (`id`),
   ADD CONSTRAINT `clubs_ibfk_1` FOREIGN KEY (`town_id`) REFERENCES `towns` (`id`),
+  ADD CONSTRAINT `clubs_ibfk_2` FOREIGN KEY (`country_id`) REFERENCES `countrys` (`id`),
   ADD CONSTRAINT `clubs_ibfk_3` FOREIGN KEY (`president_id`) REFERENCES `presidents` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `clubs_leagues`
 --
 ALTER TABLE `clubs_leagues`
-  ADD CONSTRAINT `clubs_leagues_ibfk_2` FOREIGN KEY (`league_id`) REFERENCES `leagues` (`id`),
-  ADD CONSTRAINT `clubs_leagues_ibfk_3` FOREIGN KEY (`club_id`) REFERENCES `clubs` (`id`);
+  ADD CONSTRAINT `clubs_leagues_ibfk_3` FOREIGN KEY (`club_id`) REFERENCES `clubs` (`id`),
+  ADD CONSTRAINT `clubs_leagues_ibfk_2` FOREIGN KEY (`league_id`) REFERENCES `leagues` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `clubs_trophys`
 --
 ALTER TABLE `clubs_trophys`
-  ADD CONSTRAINT `clubs_trophys_ibfk_2` FOREIGN KEY (`trophy_id`) REFERENCES `trophys` (`id`),
-  ADD CONSTRAINT `clubs_trophys_ibfk_3` FOREIGN KEY (`club_id`) REFERENCES `clubs` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `leagues`
---
-ALTER TABLE `leagues`
-  ADD CONSTRAINT `leagues_ibfk_2` FOREIGN KEY (`president_id`) REFERENCES `presidents` (`id`),
-  ADD CONSTRAINT `leagues_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`);
+  ADD CONSTRAINT `clubs_trophys_ibfk_3` FOREIGN KEY (`club_id`) REFERENCES `clubs` (`id`),
+  ADD CONSTRAINT `clubs_trophys_ibfk_2` FOREIGN KEY (`trophy_id`) REFERENCES `trophys` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `towns`
 --
 ALTER TABLE `towns`
-  ADD CONSTRAINT `towns_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`);
+  ADD CONSTRAINT `towns_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `countrys` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
